d7e28598a7b5b7cd62401ef581229f12058ce07b
MatthausZillig <matthauszigillig@hotmail.com>
2023-08-05 02:13:25 -0300

    feat: Adding moviesStore and getMovies action

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,15 @@
 <template>
   <div>
     <p text-4 text-gray-400>Home page</p>
+    <pre>{{ moviesStore.movies }}</pre>
   </div>
 </template>
 
 <script setup lang="ts">
-const { data } = await useFetch('/api/movies/discover')
-console.log('Movies', data.value)
+import { useMoviesStore } from '~/stores/MoviesStore'
+const moviesStore = useMoviesStore()
+
+moviesStore.getMovies()
 </script>
 
 <style scoped></style>
---
+++ stores/MoviesStore.ts
@@ -0,0 +1,18 @@
+import { defineStore } from 'pinia'
+import { APIResponse } from 'types/MoviesList'
+
+export const useMoviesStore = defineStore('moviesStore', {
+  state: () => ({
+    movies: [] as APIResponse[],
+    isLoading: false
+  }),
+  actions: {
+    async getMovies(): Promise<void> {
+      this.isLoading = true
+      const res = await fetch('/api/movies/discover')
+      const data = await res.json()
+      this.movies = data as unknown as APIResponse[]
+      this.isLoading = false
+    }
+  }
+})
