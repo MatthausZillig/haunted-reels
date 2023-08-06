cec30d5a9b60851c554100c338503646e0b68941
MatthausZillig <matthauszigillig@hotmail.com>
2023-08-05 22:07:42 -0300

    fix: env

--- server/api/movies/[...].ts
+++ server/api/movies/[...].ts
@@ -1,10 +1,13 @@
 export default defineEventHandler((event) => {
   const id = event.node.req?.url && [...event.node.req?.url.split('/')].pop()
   const config = useRuntimeConfig()
-  return $fetch(`${config.apiBaseUrl}/movie/${id}&include_video=true`, {
-    method: 'GET',
-    headers: {
-      Authorization: `Bearer ${config.apiToken}`
+  return $fetch(
+    `${config.apiBaseUrl}/movie/${id}&include_video=true&api_key=${config.apiKey}`,
+    {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${config.apiToken}`
+      }
     }
-  })
+  )
 })
